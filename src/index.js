ce380b7d24482c53b27869c05a39656665475a04
cherinegrove <113043328+cherinegrove@users.noreply.github.com>
2026-03-16 21:13:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // src/index.js
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const path    = require('path');
 const app     = express();
